955acd0788117dff249573679b789c963fa8b635
Bùi Minh Tuấn <buiminhtuan044@gmail.com>
2025-07-11 21:02:18 +0700

    add package

---
+++ src/main/java/com/smokingcessation/platform/entity/PackageModel.java
@@ -0,0 +1,43 @@
+package com.smokingcessation.platform.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.smokingcessation.platform.enums.PackageType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "packages")
+public class PackageModel extends BaseModel {
+    private String name;
+    private String description;
+    @Lob
+    @Column
+    private String featured;
+    private int totalDays;
+    private String imageUrl;
+    private int price;
+    private int salePrice;
+    private Double rating;
+    private int totalRating;
+    private boolean isActive = true;
+    private PackageType packageType = PackageType.BEGINNER;
+
+    @OneToMany(
+            mappedBy = "packageModel",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    @JsonBackReference
+    List<UserPackageModel> userPackages = new ArrayList<>();
+
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+}
