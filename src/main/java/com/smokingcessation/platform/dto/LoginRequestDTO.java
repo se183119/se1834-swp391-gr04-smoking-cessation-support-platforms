e3853bc3c152947267d31bb185b0b09ebcd50d8f
PhucNPH <126009035+se183119@users.noreply.github.com>
2025-07-23 21:47:26 +0700

    Update LoginRequestDTO.java

--- src/main/java/com/smokingcessation/platform/dto/LoginRequestDTO.java
+++ src/main/java/com/smokingcessation/platform/dto/LoginRequestDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 public class LoginRequestDTO {
-    private String username;
+    // private String username;
+    private String email;
     private String password;
 }
